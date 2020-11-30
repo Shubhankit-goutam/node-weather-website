dbcb74fa45e76869c2f3634ed978a003fb48d105
Shubhankit <shubhankitkumar@gmail.com>
2020-11-29 22:55:32 -1200

    init commit

---
+++ src/Utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('postman-request');
+
+
+
+const forecast = (laitude, longitude ,callback)=>{
+    const url =   'http://api.weatherstack.com/current?access_key=63c7d628523c487d4f73fd9fa4d6aa09&query=' + laitude + ' , ' + longitude + '&units=f'
+    request({url , json : true},(error, {body})=>{
+       if(error){
+         callback('Unable to connect weather service!', undefined)
+       }else if(body.error){
+  callback('Unable to find location', undefined)
+       } else{
+       
+  callback(undefined,body.current.weather_descriptions + " it is current "+ body.current.temperature + " degress out. it is feels like "+ body.current.feelslike + " % degress out")
+       }
+     }) 
+  }
+  
+  module.exports =forecast;
---
+++ src/app.js
@@ -0,0 +1,112 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./Utils/geocode')
+const forecast = require('./Utils/forecast')
+
+
+
+/* console.log(__dirname);
+console.log(path.join(__dirname,'../public'));
+ */
+const app = express()
+
+// define path for express config
+const PubliDirName = path.join(__dirname,'../public')
+const viewPath = path.join(__dirname,'../templates/views')
+const partialPath = path.join(__dirname,'../templates/partials')
+
+// setup handlebar engine and view location
+app.set('view engine','hbs')
+app.set('views',viewPath)
+hbs.registerPartials(partialPath);
+// setup static directory to serve
+app.use(express.static(PubliDirName))
+
+app.get('',(req, res)=>{
+    res.render('index',{
+        title:'Weather App',
+        name: 'Shubhankit'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About Me',
+        name: 'Shubhankit Goutam'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        helptext:'this is helpful text',
+        title: 'Help',
+        name:'Shubhankit'
+    })
+})
+
+ app.get('/Weather',(req,res)=>{ 
+    
+    if(!req.query.address){
+        return res.send({
+            error : 'Input the Location'
+        })
+    }
+    
+    geocode(req.query.address,(error, {laitude, longitude , location} = {})=>{
+        if(error){
+            return res.send({ error })
+        }
+
+        forecast(laitude, longitude, (error, forecastData)=>{
+            if(error){
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast:forecastData,
+                location,
+                address : req.query.address
+            })
+        })
+    })
+    /* res.send({
+        forecast:"It is showing ",
+        Location:"Bijnor",
+        address : req.query.address
+    }); */
+}) 
+
+app.get('/product',(req,res)=>{
+    if (!req.query.search) {
+          return res.send({
+            error:'You must providee a search term'
+          })     
+    }
+    console.log(req.query.search);
+    res.send({
+        product : []
+    })
+})
+    
+  
+
+ app.get('/help/*', (req,res)=>{
+     res.render('404',{
+         title: '404',
+         name : 'shubhankit',
+         ErrorMeassage : 'Help Article is not found'
+     })
+ })
+
+ app.get('*',(req,res)=>{
+     res.render('404',{
+         title : '404',
+         name : 'Shubhankit',
+         ErrorMeassage : 'Page not found'
+     }) 
+ })
+
+app.listen(3000,()=>{
+    console.log('server is up on port 3000.');
+})
