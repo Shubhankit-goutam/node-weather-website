2763c2dccc8643655ee87b23f7de0c612e83b379
Shubhankit <shubhankitkumar@gmail.com>
2020-11-30 23:31:26 -1200

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,17 +6,18 @@ const forecast = require('./Utils/forecast')
 
 
 
-/* console.log(__dirname);
-console.log(path.join(__dirname,'../public'));
- */
+
 const app = express()
+const port = process.env.PORT || 3000;
 
 // define path for express config
+
 const PubliDirName = path.join(__dirname,'../public')
 const viewPath = path.join(__dirname,'../templates/views')
 const partialPath = path.join(__dirname,'../templates/partials')
 
 // setup handlebar engine and view location
+
 app.set('view engine','hbs')
 app.set('views',viewPath)
 hbs.registerPartials(partialPath);
@@ -107,6 +108,6 @@ app.get('/product',(req,res)=>{
      }) 
  })
 
-app.listen(3000,()=>{
-    console.log('server is up on port 3000.');
+app.listen(port,()=>{
+    console.log('server is up on port '+ port );
 })
